9f38d145db4d236231623d18434de7fd5ff0045b
sayan mitra <sayan mitra@LAPTOP-8UJH6I3N>
2020-02-05 21:12:45 +0530

    EPAM First Task - 1

---
+++ hometask/src/main/java/hometask/NewYearGift.java
@@ -0,0 +1,53 @@
+package hometask;
+interface gift
+{
+	double getw();
+	void setw(double w);
+	double getp();
+	void setp(double p);
+
+}
+
+class sweets implements gift
+{
+	double wt;
+	double prc;
+	public void setw(double f)
+	{
+		wt=f;
+	}   
+	public double getw()
+	{
+		return wt;
+	}
+	public void setp(double p)
+	{
+		prc=p;
+	}   
+	public double getp()
+	{
+		return prc;
+	}
+  
+}
+class chocolate extends sweets
+{
+	public void display()
+	{
+		System.out.println("Weight: "+ this.getw());
+		System.out.println("Price: "+this.getp()); 
+	}
+}
+
+class ece
+{
+	public static void main(String args[])
+	{
+		chocolate obj=new chocolate();
+		double wi=8.5;
+		double pp=7.5;
+		obj.setw(wi);
+		obj.setp(pp);
+		obj.display();
+	}
+}
